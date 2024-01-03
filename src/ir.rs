2d94def1242f8717598ee99e69945dbd8ba2ec1d
Nanai Jua <me@n7i.dev>
2024-01-03 13:55:34 +0900

    feat(ir): impl `Debug`

--- src/ir.rs
+++ src/ir.rs
@@ -1,8 +1,16 @@
-pub trait Expr {}
+use core::fmt::Debug;
+
+pub trait Expr: Debug {}
 
 impl Expr for Int {}
 pub struct Int {
     pub val: usize,
+}
+
+impl Debug for Int {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "(int: {})", self.val)
+    }
 }
 
 impl Expr for Add {}
@@ -11,10 +19,22 @@ pub struct Add {
     pub rhs: Box<dyn Expr>,
 }
 
+impl Debug for Add {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "({:?} + {:?})", self.lhs, self.rhs)
+    }
+}
+
 impl Expr for Sub {}
 pub struct Sub {
     pub lhs: Box<dyn Expr>,
     pub rhs: Box<dyn Expr>,
+}
+
+impl Debug for Sub {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "({:?} - {:?})", self.lhs, self.rhs)
+    }
 }
 
 impl Expr for Mul {}
@@ -23,14 +43,32 @@ pub struct Mul {
     pub rhs: Box<dyn Expr>,
 }
 
+impl Debug for Mul {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "({:?} * {:?})", self.lhs, self.rhs)
+    }
+}
+
 impl Expr for Div {}
 pub struct Div {
     pub lhs: Box<dyn Expr>,
     pub rhs: Box<dyn Expr>,
 }
 
+impl Debug for Div {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "({:?} / {:?})", self.lhs, self.rhs)
+    }
+}
+
 impl Expr for Mdl {}
 pub struct Mdl {
     pub lhs: Box<dyn Expr>,
     pub rhs: Box<dyn Expr>,
+}
+
+impl Debug for Mdl {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "({:?} % {:?})", self.lhs, self.rhs)
+    }
 }
